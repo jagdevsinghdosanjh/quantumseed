debb48796ed6582d5fb6081ad47775afc021c6aa
jagdevsinghdosanjh <jagdevsinghdosanjh@gmail.com>
2025-10-01 22:41:23 +0530

    Ok

--- quantumseed_app.py
+++ quantumseed_app.py
@@ -17,7 +17,7 @@ st.markdown("""
 </div>
 """, unsafe_allow_html=True)
 
-with open("styles/poetic_theme.css") as f:
+with open("quantumseed/styles/poetic_theme.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 # Sidebar: Educator Toggles
